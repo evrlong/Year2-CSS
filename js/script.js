57ccc4ef1f480303680b62ab6403d15cc3c006ee
evrlong <127796398+evrlong@users.noreply.github.com>
2025-05-14 18:57:39 +0200

    Update script.js
    Removed unused code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-// import { loadFooter } from '../components/footer.js';
+// // import { loadFooter } from '../components/footer.js';
 
-// const menuToggle = document.getElementById('menu-toggle');
-// const mobileMenu = document.getElementById('mobile-menu');
+// // Get all links
+// const links = document.querySelectorAll('.nav-link');
 
-// // menuToggle.addEventListener('click', () => {
-// //   mobileMenu.classList.toggle('hidden');
-// // });
+// // Henter nåværende URL-filnavn
+// const currentPage = window.location.pathname.split('/').pop();
 
-// Henter alle lenkene
-const links = document.querySelectorAll('.nav-link');
-// Henter nåværende URL-filnavn
-const currentPage = window.location.pathname.split('/').pop();
-
-links.forEach((link) => {
-  if (link.getAttribute('href') === currentPage) {
-    link.classList.add('border-blue-500');
-  }
-});
+// links.forEach((link) => {
+//   if (link.getAttribute('href') === currentPage) {
+//     link.classList.add('border-blue-500');
+//   }
+// });
